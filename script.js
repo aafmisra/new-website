4d09c24ea36d2147d80a5f5aa60b364e4cf5d106
Asha M. Wrenn <aafmisra@gmail.com>
2020-08-23 13:47:38 -0400

    expanded header with underline, event listeners for icons

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
 const logo = document.querySelector('#logo');
 
+const blink = '/static/img/icon_peace.svg';
+const awake = '/static/img/icon_smile.svg';
+const happy = '/static/img/icon_laugh.svg'
+
 setTimeout(function() {
-    logo.setAttribute('src', '/static/img/icon_smile.svg');
+    logo.setAttribute('src', awake);
 }, 1000);
 
 setTimeout(function() {
-    logo.setAttribute('src', '/static/img/icon_peace.svg');
+    logo.setAttribute('src', blink);
 }, 1100);
 
 setTimeout(function() {
-    logo.setAttribute('src', '/static/img/icon_smile.svg');
+    logo.setAttribute('src', awake);
 }, 1200);
 
-const blink = setInterval(function() {
-    logo.setAttribute('src', '/static/img/icon_peace.svg');
-    console.log('blink', new Date())
-}, 5000);
-
-const openEyes = setInterval(function() {
-    logo.setAttribute('src', '/static/img/icon_smile.svg');
-    console.log('open', new Date())
-}, 5100);
 
 const blog = document.querySelector('#blog');
-const email = document.querySelector('#logo');
+const email = document.querySelector('#email');
 const github = document.querySelector('#github');
 const linkedin = document.querySelector('#linkedin');
 
-blog.addEventListener('mouseOver', clearInterval(blink))
-blog.addEventListener('mouseOver', clearInterval(openEyes))
+blog.addEventListener('mouseenter', function() {
+    logo.setAttribute('src', happy)
+})
+blog.addEventListener('mouseleave', function() {
+    logo.setAttribute('src', awake)
+})
 
-email.addEventListener('mouseOver', clearInterval(blink))
-email.addEventListener('mouseOver', clearInterval(openEyes))
+email.addEventListener('mouseenter', function() {
+    logo.setAttribute('src', happy)
+})
+email.addEventListener('mouseleave', function() {
+    logo.setAttribute('src', awake)
+})
 
-github.addEventListener('mouseOver', clearInterval(blink))
-github.addEventListener('mouseOver', clearInterval(openEyes))
+github.addEventListener('mouseenter', function() {
+    logo.setAttribute('src', happy)
+})
+github.addEventListener('mouseleave', function() {
+    logo.setAttribute('src', awake)
+})
 
-linkedin.addEventListener('mouseOver', clearInterval(blink))
-linkedin.addEventListener('mouseOver', clearInterval(openEyes))
+linkedin.addEventListener('mouseenter', function() {
+    logo.setAttribute('src', happy)
+})
+linkedin.addEventListener('mouseleave', function() {
+    logo.setAttribute('src', awake)
+})
 
 
